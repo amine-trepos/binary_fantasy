0d363a7c0c8792255a5c6c13359143ee61bd4d28
amine-trepos <104711847+amine-trepos@users.noreply.github.com>
2022-05-01 21:00:17 +0900

    Add license notice

--- read_binary_fantasy.c
+++ read_binary_fantasy.c
@@ -1,3 +1,21 @@
+/* Copyright 2022 amine-trepos
+ *
+ * This file is part of binary_fantasy.
+ *
+ * binary_fantasy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * binary_fantasy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with binary_fantasy.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 # include <stdio.h>
 # include <time.h>
 # include <string.h>
